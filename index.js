f6cfbddda2f09d524149654f8d4047a450203b40
Aninditaroy <anindita.roy.mu@gmail.com>
2022-07-21 19:26:49 +0600

    students edit and delete api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000
 const app = express();
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Credentials", "true");;
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -35,6 +36,52 @@ async function run() {
         app.post('/students', async (req, res) => {
             const student = req.body;
             const result = await studentsCollection.insertOne(student);
+            res.send(result);
+        })
+
+        // get api with id for student
+        app.get('/students/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const student = await studentsCollection.findOne(query);
+            res.send(student);
+        })
+
+        // edit students
+        app.put('/students/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStudent = req.body;
+            console.log(updatedStudent)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+
+            const updateDoc = {
+                $set: {
+                    firstName: updatedStudent.firstName,
+                    lastName: updatedStudent.lastName,
+                    birthDate: updatedStudent.birthDate,
+                    gender: updatedStudent.gender,
+                    email: updatedStudent.email,
+                    phoneNumber: updatedStudent.phoneNumber,
+                    grade: updatedStudent.grade,
+                    studentId: updatedStudent.studentId,
+                    semester: updatedStudent.semester,
+                    address: updatedStudent.address,
+                    // img: updatedStudent.img,
+                },
+            };
+            const result = await studentsCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+
+        // delete a student
+        app.delete('/students/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            };
+            const result = await studentsCollection.deleteOne(query);
             res.send(result);
         })
 
